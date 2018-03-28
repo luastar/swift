7d2912e0667fb1fe949963b8b62f3b02380f4772
祝明华 <minghua.zhu@fenbeitong.com>
2018-03-28 14:30:26 +0800

    增加下画线和驼峰互转json的方法

--- swift-base/src/main/java/com/luastar/swift/base/json/JsonUtils.java
+++ swift-base/src/main/java/com/luastar/swift/base/json/JsonUtils.java
@@ -1,5 +1,9 @@
 package com.luastar.swift.base.json;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.fasterxml.jackson.databind.JavaType;
 
 import java.util.Collection;
@@ -9,6 +13,16 @@ public abstract class JsonUtils {
 
     protected static JsonMapper jsonMapper = new JsonMapper();
 
+    private static SerializeConfig serializeConfig;
+    private static ParserConfig parserConfig;
+
+    static {
+        serializeConfig = new SerializeConfig();
+        serializeConfig.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
+        parserConfig = new ParserConfig();
+        parserConfig.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
+    }
+
     public static JsonMapper getJsonMapper() {
         return jsonMapper;
     }
@@ -17,8 +31,16 @@ public abstract class JsonUtils {
         return jsonMapper.toJson(obj);
     }
 
+    public static String toJsonSnake(Object obj) {
+        return JSON.toJSONString(obj, serializeConfig);
+    }
+
     public static <T> T toObj(String json, Class<T> clazz) {
         return jsonMapper.toObj(json, clazz);
+    }
+
+    public static <T> T toObjSnake(String json, Class<T> clazz) {
+        return JSON.parseObject(json, clazz, parserConfig);
     }
 
     public static <T> T toObj(String json, Class<? extends Collection> collectionClass, Class<?> elementClass) {
