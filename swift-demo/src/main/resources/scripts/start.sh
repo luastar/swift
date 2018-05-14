a412faf3b716da525494dc173919c3de17f9731d
祝明华 <minghua.zhu@fenbeitong.com>
2018-05-14 16:56:48 +0800

    启动脚本优化

--- swift-demo/src/main/resources/scripts/start.sh
+++ swift-demo/src/main/resources/scripts/start.sh
@@ -1,16 +1,64 @@
 #!/bin/bash
 
-#JMX_PORT=`expr $1 + 500`
-JAVA_OPTS="-server -Xms1024m -Xmx1024m -Xmn384m -XX:MaxPermSize=128m \
--Xss1m -XX:+UseConcMarkSweepGC \
--XX:+UseParNewGC -XX:CMSFullGCsBeforeCompaction=5 \
--XX:+UseCMSCompactAtFullCollection \
--XX:+PrintGC -Xloggc:/data/logs/${APP}/gc_$1.log"
-#-Djava.rmi.server.hostname=114.215.143.54 \
-#-Dcom.sun.management.jmxremote \
-#-Dcom.sun.management.jmxremote.port=$JMX_PORT \
-#-Dcom.sun.management.jmxremote.authenticate=false \
-#-Dcom.sun.management.jmxremote.ssl=false"
+HOST_NAME=`ifconfig eth0 | grep "inet addr" | cut -f 2 -d ":" | cut -f 1 -d " "`
+JMX_PORT=`expr $1 + 500`
+#jdk 1.7
+#JAVA_OPTS="
+# -Xmx2G \
+# -Xms2G \
+# -Xmn768M \
+# -XX:PermSize=64M \
+# -XX:MaxPermSize=256M \
+# -Xss1M \
+# -XX:+DisableExplicitGC \
+# -XX:SurvivorRatio=1 \
+# -XX:+UseConcMarkSweepGC \
+# -XX:+UseParNewGC \
+# -XX:+CMSParallelRemarkEnabled \
+# -XX:+UseCMSCompactAtFullCollection \
+# -XX:CMSFullGCsBeforeCompaction=0 \
+# -XX:+CMSClassUnloadingEnabled \
+# -XX:LargePageSizeInBytes=128M \
+# -XX:+UseFastAccessorMethods \
+# -XX:+UseCMSInitiatingOccupancyOnly \
+# -XX:CMSInitiatingOccupancyFraction=80 \
+# -XX:SoftRefLRUPolicyMSPerMB=0 \
+# -XX:+PrintClassHistogram \
+# -XX:+PrintGCDetails \
+# -XX:+PrintGCTimeStamps \
+# -XX:+PrintHeapAtGC \
+# -Xloggc:/tmp/gc.log"
+
+#jdk 1.8
+JAVA_OPTS="
+-server \
+-Xms2g \
+-Xmx2g \
+-Xmn768m \
+-XX:MetaspaceSize=64m \
+-XX:MaxMetaspaceSize=256m \
+-Xss1m \
+-XX:+DisableExplicitGC \
+-XX:+UseConcMarkSweepGC \
+-XX:+CMSParallelRemarkEnabled \
+-XX:LargePageSizeInBytes=128m \
+-XX:+UseFastAccessorMethods \
+-XX:+UseCMSInitiatingOccupancyOnly \
+-XX:CMSInitiatingOccupancyFraction=70 \
+-XX:SoftRefLRUPolicyMSPerMB=0 \
+-XX:+UnlockCommercialFeatures \
+-XX:+FlightRecorder \
+-Djava.rmi.server.hostname=$HOST_NAME \
+-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.port=$JMX_PORT \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-XX:+PrintClassHistogram \
+-XX:+PrintGCDetails \
+-XX:+PrintGCTimeStamps \
+-XX:+PrintHeapAtGC \
+-Xloggc:/data/logs/stereo/gc_$1.log
+"
 
 SCRIPT_HOME=$(dirname $(readlink -f $0))
 PROJECT_HOME=$(dirname ${SCRIPT_HOME})
