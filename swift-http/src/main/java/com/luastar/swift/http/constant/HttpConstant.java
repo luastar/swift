7bdc5c0abdfbe9056742093892e9d743e76cb9d0
祝明华 <minghua.zhu@fenbeitong.com>
2018-06-20 18:44:53 +0800

    业务线程池优化

--- swift-http/src/main/java/com/luastar/swift/http/constant/HttpConstant.java
+++ swift-http/src/main/java/com/luastar/swift/http/constant/HttpConstant.java
@@ -93,9 +93,13 @@ public interface HttpConstant {
      */
     int SWIFT_WORKER_THREADS = PropertyUtils.getInt("swift.workerThreads", 0);
     /**
-     * 业务线程数，默认值 64
+     * 业务核心线程数，默认值 16
      */
-    int SWIFT_BUSINESS_THREADS = PropertyUtils.getInt("swift.businessThreads", 64);
+    int SWIFT_BUSINESS_CORE_THREADS = 16;
+    /**
+     * 业务最大线程数，默认值 64
+     */
+    int SWIFT_BUSINESS_MAX_THREADS = PropertyUtils.getInt("swift.businessThreads", 64);
     /**
      * 返回结果压缩级别，0~9，默认6
      */
