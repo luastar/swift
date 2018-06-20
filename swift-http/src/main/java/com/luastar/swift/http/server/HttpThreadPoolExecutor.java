132208dbaf8f9cffe68297a81a9c98aef3506031
祝明华 <minghua.zhu@fenbeitong.com>
2018-06-20 18:47:57 +0800

    业务线程池优化

--- swift-http/src/main/java/com/luastar/swift/http/server/HttpThreadPoolExecutor.java
+++ swift-http/src/main/java/com/luastar/swift/http/server/HttpThreadPoolExecutor.java
@@ -69,21 +69,19 @@ public class HttpThreadPoolExecutor {
      * @return
      */
     public static Future<?> submit(String requestId, Runnable task) {
-        if (logger.isDebugEnabled()) {
-            StringBuilder info = new StringBuilder()
-                    .append("当前业务线程池信息：").append("\n")
-                    .append("============================================================").append("\n")
-                    .append("== queueSize(线程队列大小) : ").append(getMainThreadPoolExecutor().getQueue().size()).append("\n")
-                    .append("== queueRemainingCapacity(线程队列剩余) : ").append(getMainThreadPoolExecutor().getQueue().remainingCapacity()).append("\n")
-                    .append("== corePoolSize(核心线程数) : ").append(getMainThreadPoolExecutor().getCorePoolSize()).append("\n")
-                    .append("== maxPoolSize(最大线程数) : ").append(getMainThreadPoolExecutor().getMaximumPoolSize()).append("\n")
-                    .append("== poolSize(当前线程数) : ").append(getMainThreadPoolExecutor().getPoolSize()).append("\n")
-                    .append("== activeCount(活动线程数) : ").append(getMainThreadPoolExecutor().getActiveCount()).append("\n")
-                    .append("== taskCount(总任务数) : ").append(getMainThreadPoolExecutor().getTaskCount()).append("\n")
-                    .append("== completedTaskCount(已完成任务数) : ").append(getMainThreadPoolExecutor().getCompletedTaskCount()).append("\n")
-                    .append("============================================================").append("\n");
-            logger.info(info.toString());
-        }
+        StringBuilder info = new StringBuilder()
+                .append("当前业务线程池信息：").append("\n")
+                .append("============================================================").append("\n")
+                .append("== queueSize(线程队列大小) : ").append(getMainThreadPoolExecutor().getQueue().size()).append("\n")
+                .append("== queueRemainingCapacity(线程队列剩余) : ").append(getMainThreadPoolExecutor().getQueue().remainingCapacity()).append("\n")
+                .append("== corePoolSize(核心线程数) : ").append(getMainThreadPoolExecutor().getCorePoolSize()).append("\n")
+                .append("== maxPoolSize(最大线程数) : ").append(getMainThreadPoolExecutor().getMaximumPoolSize()).append("\n")
+                .append("== poolSize(当前线程数) : ").append(getMainThreadPoolExecutor().getPoolSize()).append("\n")
+                .append("== activeCount(活动线程数) : ").append(getMainThreadPoolExecutor().getActiveCount()).append("\n")
+                .append("== taskCount(总任务数) : ").append(getMainThreadPoolExecutor().getTaskCount()).append("\n")
+                .append("== completedTaskCount(已完成任务数) : ").append(getMainThreadPoolExecutor().getCompletedTaskCount()).append("\n")
+                .append("============================================================").append("\n");
+        logger.info(info.toString());
         Future<?> future = getMainThreadPoolExecutor().submit(task);
         getKillThreadPoolExecutor().submit(() -> {
             try {
