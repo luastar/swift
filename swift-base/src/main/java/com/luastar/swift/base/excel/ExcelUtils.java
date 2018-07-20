0da0e321009e79ced46bdd25854a93bba3187a97
祝明华 <minghua.zhu@fenbeitong.com>
2018-07-20 23:39:39 +0800

    捕获获取属性值异常，取不到即为空

--- swift-base/src/main/java/com/luastar/swift/base/excel/ExcelUtils.java
+++ swift-base/src/main/java/com/luastar/swift/base/excel/ExcelUtils.java
@@ -22,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.LinkedHashMap;
