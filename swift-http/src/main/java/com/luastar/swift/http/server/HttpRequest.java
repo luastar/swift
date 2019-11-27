4d8eaba40232421802e8c8d9779cd97543f5dbb7
祝明华 <minghua.zhu@fenbeitong.com>
2019-11-27 18:07:02 +0800

    将非线程安全的 format 类换成线程安全的

--- swift-http/src/main/java/com/luastar/swift/http/server/HttpRequest.java
+++ swift-http/src/main/java/com/luastar/swift/http/server/HttpRequest.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.luastar.swift.base.entity.SwiftHashMap;
-import com.luastar.swift.base.utils.DateUtils;
 import com.luastar.swift.base.utils.ObjUtils;
 import com.luastar.swift.http.constant.HttpConstant;
 import io.netty.buffer.ByteBuf;
@@ -24,6 +23,7 @@ import org.springframework.validation.DataBinder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -380,7 +380,7 @@ public class HttpRequest {
 
     public <T> T bindObj(T obj) {
         DataBinder dataBinder = new DataBinder(obj);
-        dataBinder.registerCustomEditor(Date.class, new CustomDateEditor(DateUtils.NORMAL_FORMAT, true));
+        dataBinder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));
         dataBinder.bind(new MutablePropertyValues(parameterMap));
         return obj;
     }
