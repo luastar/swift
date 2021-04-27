ef10ab77fad882ecf57c453d6647def0e1166e24
祝明华 <minghua.zhu@fenbeitong.com>
2021-04-27 11:59:40 +0800

    no message

--- swift-base/src/main/java/com/luastar/swift/base/utils/ObjUtils.java
+++ swift-base/src/main/java/com/luastar/swift/base/utils/ObjUtils.java
@@ -170,7 +170,7 @@ public class ObjUtils {
         String strValue = obj.toString().trim();
         try {
             Number number = NumberFormat.getNumberInstance().parse(strValue);
-            return new BigDecimal(number.doubleValue());
+            return new BigDecimal(number.toString());
         } catch (Exception e) {
         }
         return null;
@@ -287,6 +287,11 @@ public class ObjUtils {
             longList.add(toLong(strAry[i], defaultValue));
         }
         return longList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ObjUtils.toBigDecimal("0.35"));
+        System.out.println(ObjUtils.toBigDecimal("123,456.89"));
     }
 
 }
